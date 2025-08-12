0dda3ad27ae99b3807e0b57ce24076379c93be27
maxburs <maxwellburson@gmail.com>
2025-08-11 23:27:26 -0700

    Fabric Fabaricasdfase

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,44 @@ import {
   MessageBar,
   MessageBarBody,
 } from '@fluentui/react-components';
-
-export const MessageBarError = ({ message }) => (
-  <MessageBar intent="error">
-    <MessageBarBody>{message}</MessageBarBody>
-  </MessageBar>
-);
+import './index.css'
 
 const App = () => {
-  return <MessageBarError message="Message bar 1" />;
+  const [messageComponent, setMessageComponent] = React.useState('message-bar');
+  const [message, setMessage] = React.useState('Hello')
+  const alertMessage = 'Alert ' + message;
+
+  return <>
+    <label>
+      Component type
+      <select defaultValue={messageComponent} onChange={e => setMessageComponent(e.target.value)}>
+        <option value='message-bar'>{'<MessageBar intent="error">'}</option>
+        <option value='aria-live'>{'<div aria-live="assertive >'}</option>
+      </select>
+    </label>
+
+
+    <label>
+      Message text
+      <select defaultValue={message} onChange={e => setMessage(e.target.value)}>
+        <option value='Hello'>Hello</option>
+        <option value='Fabric Fabric Fabric'>Fabric Fabric Fabric</option>
+      </select>
+    </label>
+
+
+    {/* <label>
+      Message text
+      <input defaultValue={message} onChange={e => setMessage(e.target.value)} />
+    </label> */}
+    {
+      messageComponent === 'message-bar'
+        ? <MessageBar intent="error">
+          <MessageBarBody>{alertMessage}</MessageBarBody>
+        </MessageBar>
+        : <div aria-live="assertive">{alertMessage}</div>
+    }
+  </>;
 };
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
